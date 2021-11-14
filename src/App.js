c059a98ab8cb5891a8945af0fd583a1d320dd5cf
Dima Rui <ruy.dima@gmail.com>
2021-11-15 01:38:22 +0200

    add preloader during initializing app

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ class App extends Component {
     }
 
     render() {
-        /*if(!this.props.initialized)
-            return <Preloader/>*/
+        if(!this.props.initialized)
+            return <Preloader/>
 
         return (
             <div className='app-wrapper'>
